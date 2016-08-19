719303c2511f3f5f597025ffc743411fa174f798
Walter Sobral <wsa@cin.ufpe.br>
2016-08-19 10:24:10 +0000

    Added setup and requirements-test files.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='creamcracker',
+    version='0.1',
+    description='Set of utils for scraping tasks with python.',
+    author='Walter Sobral',
+    author_email='wsacin@gmail.com',
+    keywords=['captcha', 'deathbycaptcha'],
+    packages=['creamcracker'],
+    )
