4336b1d4a72e1084f9a069610a8c0305c0a08b42
Arthur Rodrigues Moreno <arm@cin.ufpe.br>
2017-11-22 14:57:51 -0300

    [coverage] Adiciona caso de multiple coverage files no metodo de raw_value.

--- scrapingutils/spidercoverage/utils.py
+++ scrapingutils/spidercoverage/utils.py
@@ -5,4 +5,6 @@ def raw_value(coverage_dict):
     for key, init_value in coverage_dict.items():
         if isinstance(init_value, enum.Enum):
             coverage_dict.update({key: init_value.value})
+        elif isinstance(init_value, dict):
+            coverage_dict.update({key: raw_value(init_value)})
     return coverage_dict
