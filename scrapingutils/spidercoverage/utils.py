ded161575bcdb0350e7e5123cdc68f97a3373405
Arthur Rodrigues Moreno <arm@cin.ufpe.br>
2017-11-22 18:31:16 -0300

    [coverage] Adiciona a classe SpiderDataCoverage no utils.

--- scrapingutils/spidercoverage/utils.py
+++ scrapingutils/spidercoverage/utils.py
@@ -1,4 +1,6 @@
 import enum
+import json
+import yaml
 
 
 def raw_value(coverage_dict):
@@ -8,3 +10,23 @@ def raw_value(coverage_dict):
         elif isinstance(init_value, dict):
             coverage_dict.update({key: raw_value(init_value)})
     return coverage_dict
+
+
+class SpiderDataCoverage(object):
+
+    def __init__(self, yaml_file, spiders):
+        self._results = None
+        self.spiders = spiders
+        self._yaml_file = yaml_file
+
+    @property
+    def results(self):
+        if not self._results:
+            self._results = []
+            for spider_cls in self.spiders:
+                self._results.append(json.loads(spider_cls.output_json()))
+        return self._results
+
+    def export_spider_coverage_yaml(self):
+        with open(self._yaml_file, 'w') as yaml_file:
+            yaml.dump(self.results, yaml_file, default_flow_style=False)
