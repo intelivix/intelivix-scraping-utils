4b1659498332aacca20111e19d142e6f96bcd317
Arthur Rodrigues Moreno <arm@cin.ufpe.br>
2017-11-22 14:26:30 -0300

    [coverage] Corrige raw_value.

--- scrapingutils/spidercoverage/utils.py
+++ scrapingutils/spidercoverage/utils.py
@@ -2,7 +2,7 @@ import enum
 
 
 def raw_value(coverage_dict):
-    for key, init_value in x.items():
+    for key, init_value in coverage_dict.items():
         if type(init_value) == enum.EnumMeta:
             coverage_dict.update({key: init_value.value})
     return coverage_dict
