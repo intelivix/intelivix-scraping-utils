15134e1895622a6d797afe842e6f3e7710fa26e0
Arthur <arthurmoreno_6@hotmail.com>
2017-11-21 21:17:52 -0300

    [coverage] Refatora scraping utils para ser um pacote geral de utilidades contendo o creamcracker.

--- setup.py
+++ setup.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='creamcracker',
+    name='scrapingutils',
     version='0.1',
     description='Set of utils for scraping tasks with python.',
     author='Walter Sobral',
     author_email='wsacin@gmail.com',
-    keywords=['captcha', 'deathbycaptcha'],
-    packages=['creamcracker', 'spidercoverage'],
-    )
-
-setup(
-    name='spidercoverage',
-    version='0.1',
-    description='Set utils for spider coverage.',
-    author='Arthur Moreno',
-    author_email='arthur.moreno@intelivix.com',
-    keywords=['spider', 'coverage'],
-    packages=['spidercoverage'],
+    keywords=['captcha', 'deathbycaptcha', 'coverage'],
+    packages=find_packages(),
     )
